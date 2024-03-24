b55284790c5c9bd66d0460c8bb4555f49fcb7bc6
dudurb17 <eduardo.robettibedin@gmail.com>
2024-03-24 19:45:43 -0300

    options de todas as telas

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "../paginas/Home";
+import Login from "../paginas/Login";
+
+const Stack = createStackNavigator();
 
 const Route = () => {
-  return <NavigationContainer></NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default Route;
